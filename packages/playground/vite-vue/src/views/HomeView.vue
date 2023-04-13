79454d6ac52af5e0204d581c0b5b2902b2d56f2d
yuguaa <280722781@qq.com>
2023-04-13 14:47:02 +0800

    playground

--- packages/playground/vite-vue/src/views/HomeView.vue
+++ packages/playground/vite-vue/src/views/HomeView.vue
@@ -1,9 +1,19 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <button @click="handleClick">点击发送Login</button>
   </main>
 </template>
+
+<script setup lang="ts">
+import { Login } from "@yugu/utils";
+const loginFn = new Login({
+  corpCode: "jinj",
+  account: "yzj",
+  password: "Yzj123456",
+  stage: "http://cloud.21tb.com",
+  mode: import.meta.env.MODE,
+});
+const handleClick = () => {
+  loginFn.login();
+};
+</script>
--- packages/playground/vite-vue/vite.config.ts
+++ packages/playground/vite-vue/vite.config.ts
@@ -1,14 +1,27 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      "/tbc-nest": {
+        target: "http://21tb-vuelibrary.21tb.com/tbc-nest",
+        changeOrigin: true,
+      },
+      "/login": {
+        target: "http://cloud.21tb.com",
+        changeOrigin: true,
+      },
+    },
+  },
+});
