9f785e65fc1a219ccf611012b27432a25ca6417d
yuguaa <280722781@qq.com>
2023-05-09 14:41:46 +0800

    feat:request

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -1,3 +1,4 @@
 // 引入login模块,并导出
 import Login from './login'
-export { Login }
+import YuguAxios from './yuguAxios'
+export { Login, YuguAxios }
---
+++ packages/core/src/yuguAxios/index.js
@@ -0,0 +1,77 @@
+import axios from 'axios'
+
+export class YuguAxios {
+  static axiosInstance = null
+  constructor(options) {
+    this.createAxios(options)
+  }
+
+  createAxios(options) {
+    this.axiosInstance = axios.create(options)
+    this.options = options
+    this.setupInterceptors()
+  }
+
+  setupInterceptors() {
+    const { interceptors } = this.options
+    if (!interceptors) return
+    const { requestInterceptors, requestInterceptorsCatch, responseInterceptors, responseInterceptorsCatch } =
+      interceptors
+
+    this.axiosInstance.interceptors.request.use(config => {
+      config = requestInterceptors(config, this.options)
+      return config
+    })
+
+    if (requestInterceptorsCatch) {
+      this.axiosInstance.interceptors.request.use(undefined, requestInterceptorsCatch)
+    }
+
+    this.axiosInstance.interceptors.response.use(response => {
+      response = responseInterceptors(response)
+      return response
+    })
+
+    if (responseInterceptorsCatch) {
+      this.axiosInstance.interceptors.response.use(undefined, error => responseInterceptorsCatch(error))
+    }
+  }
+
+  get(config, options) {
+    return this.request({ ...config, method: 'GET' }, options)
+  }
+
+  post(config, options) {
+    return this.request({ ...config, method: 'POST' }, options)
+  }
+
+  put(config, options) {
+    return this.request({ ...config, method: 'PUT' }, options)
+  }
+
+  delete(config, options) {
+    return this.request({ ...config, method: 'DELETE' }, options)
+  }
+
+  request(config, options) {
+    let conf = config
+    const { requestOptions } = this.options
+    const opt = Object.assign({}, requestOptions, options)
+    conf.requestOptions = opt
+    conf.headers = {
+      'Content-Type': 'application/json;charset=UTF-8',
+      ...conf.headers,
+      ...opt.headers
+    }
+    return new Promise((resolve, reject) => {
+      this.axiosInstance
+        .request(conf)
+        .then(res => {
+          resolve(res)
+        })
+        .catch(e => {
+          reject(e)
+        })
+    })
+  }
+}
